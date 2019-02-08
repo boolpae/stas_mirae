467265aa05e11fdda53d2386554c2139293e34f6
이승훈 - 개인노트북 <boolpae@naver.com>
2019-02-08 13:49:31 +0900

    codecvt를 boost로 교체

--- src/stas/Utils.cpp
+++ src/stas/Utils.cpp
@@ -7,7 +7,42 @@
 #include <regex>
 #include <algorithm>
 #include <locale>
+#if 0
 #include <codecvt>
+#else
+#include <boost/locale/encoding_utf.hpp>
+#endif
+
+using namespace std;
+using boost::locale::conv::utf_to_utf;
+
+#if 0
+wstring s2ws(const std::string& str)
+{
+    using convert_typeX = std::codecvt_utf8<wchar_t>;
+    std::wstring_convert<convert_typeX, wchar_t> converterX;
+
+    return converterX.from_bytes(str);
+}
+
+string ws2s(const std::wstring& wstr)
+{
+    using convert_typeX = std::codecvt_utf8<wchar_t>;
+    std::wstring_convert<convert_typeX, wchar_t> converterX;
+
+    return converterX.to_bytes(wstr);
+}
+#else
+std::wstring s2ws(const std::string& str)
+{
+    return utf_to_utf<wchar_t>(str.c_str(), str.c_str() + str.size());
+}
+
+std::string ws2s(const std::wstring& str)
+{
+    return utf_to_utf<char>(str.c_str(), str.c_str() + str.size());
+}  
+#endif
 
 void MakeDirectory(const char *full_path)
 {
@@ -25,23 +60,6 @@ void MakeDirectory(const char *full_path)
     }
         sp++; // 포인터를 다음 문자로 이동
     }
-}
-
-using namespace std;
-wstring s2ws(const std::string& str)
-{
-    using convert_typeX = std::codecvt_utf8<wchar_t>;
-    std::wstring_convert<convert_typeX, wchar_t> converterX;
-
-    return converterX.from_bytes(str);
-}
-
-string ws2s(const std::wstring& wstr)
-{
-    using convert_typeX = std::codecvt_utf8<wchar_t>;
-    std::wstring_convert<convert_typeX, wchar_t> converterX;
-
-    return converterX.to_bytes(wstr);
 }
 
 void remSpaceInSentence(string& org)
