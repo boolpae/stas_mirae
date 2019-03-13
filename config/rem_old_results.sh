878f12892ddec004c4b574c2d3f357736d6ca775
이승훈 - 개인노트북 <boolpae@naver.com>
2019-03-13 18:57:04 +0900

    오래된 결과 삭제 스크립트 수정

--- config/rem_old_results.sh
+++ config/rem_old_results.sh
@@ -2,10 +2,11 @@
 
 LOGFILE="/home/stt/Smart-VR/logs/rem_old_result.log"
 LOGFILE="rem_old_result.log"
-OFFSET=30
-WAVPATH="/home/boolpae/TEMP/WAVS/"
-RT_STTPATH="/home/boolpae/TEMP/STT/REALTIME/"
-FN_STTPATH="/home/boolpae/TEMP/STT/FILETIME/"
+# 2달 - 60일
+OFFSET=60
+WAVPATH="/home/stt/Smart-VR/WAVS/"
+RT_STTPATH="/home/stt/Smart-VR/STT/REALTIME/"
+FN_STTPATH="/home/stt/Smart-VR/STT/FILETIME/"
 
 RMDIR='/bin/rm'
 RMOPT='-rf'
@@ -15,16 +16,16 @@ RDATE=`date +"%Y%m%d" -d "${OFFSET} day ago"`
 #echo ${RDATE}
 
 # remove directory
+echo $(date) "- [START] remove directory(${WAVPATH}${RDATE})" >> ${LOGFILE}
 if [ -d ${WAVPATH}${RDATE} ]; then
-	echo $(date) "- [START] remove directory(${WAVPATH}${RDATE})" >> ${LOGFILE}
 	${RMDIR} ${RMOPT} ${WAVPATH}${RDATE}
 fi
+echo $(date) "- [START] remove directory(${RT_STTPATH}${RDATE})" >> ${LOGFILE}
 if [ -d ${RT_STTPATH}${RDATE} ]; then
-	echo $(date) "- [START] remove directory(${RT_STTPATH}${RDATE})" >> ${LOGFILE}
 	${RMDIR} ${RMOPT} ${RT_STTPATH}${RDATE}
 fi
+echo $(date) "- [START] remove directory(${FN_STTPATH}${RDATE})" >> ${LOGFILE}
 if [ -d ${FN_STTPATH}${RDATE} ]; then
-	echo $(date) "- [START] remove directory(${FN_STTPATH}${RDATE})" >> ${LOGFILE}
 	${RMDIR} ${RMOPT} ${FN_STTPATH}${RDATE}
 fi
 
